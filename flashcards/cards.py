129dc55d96c78ccf95334aa3d141a02446fa7897
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2016-05-14 23:43:14 -0400

    Add question and answer property to QuestionCard.

--- flashcards/cards.py
+++ flashcards/cards.py
@@ -1,9 +1,68 @@
 class BaseCard(object):
     """
-    Basic class representing a question card.
+    Basic class representing a card.
     """
 
-    def __init__(self, question, answer):
 
+class QuestionCard(BaseCard):
+    """
+    Class representing a question card.
+
+    A question card is simple card containing a Question and an Answer.
+    """
+    def __init__(self, question, answer):
+        """
+        Creates a Question card containing a question and an answer to the
+        question.
+
+        :param question: The question of the card.
+        :param answer: The answer to the question.
+        """
         self._question = question
         self._answer = answer
+
+    @property
+    def question(self):
+        """
+        Get the question of the card.
+
+        :returns: The question of the card.
+        """
+        return self._question
+
+    @question.setter
+    def question(self, value):
+        """
+        Set a new value for the question of this card
+
+        :param new_question: The new question
+
+        :raises TypeError: If the question is not of type str
+        """
+        if isinstance(value, basestring):
+            self._question = value
+        else:
+            raise TypeError('Question should be of type str')
+
+    @property
+    def answer(self):
+        """
+        Get the answer for the question
+
+        :returns: The answer to the question.
+        """
+        return self._answer
+
+    @answer.setter
+    def answer(self, value):
+        """
+        Set a new value for the question of this card
+
+        :param new_question: The new question
+
+        :raises BadQuestionErr: If the question is not of type str
+        """
+        if isinstance(value, basestring):
+            self._answer = value
+        else:
+            raise TypeError('Answer should be of type str')
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,8 +1,35 @@
 import unittest
 
-from flashcards.cards import BaseCard
+from flashcards.cards import QuestionCard
 
-class TestCards(unittest.TestCase):
+class TestQuestionCards(unittest.TestCase):
+
+    def setUp(self):
+        self.card = QuestionCard('what is PI ?', '3.14159265359')
 
     def test_Card_class_exists(self):
-        self.assertIsNotNone(BaseCard('meaning of life?', '42'))
+        self.assertIsNotNone(QuestionCard('meaning of life?', '42'))
+
+    def test_card_get_question(self):
+        self.assertEqual('what is PI ?', self.card.question)
+
+    def test_card_get_answer(self):
+        self.assertEqual('3.14159265359', self.card.answer)
+
+    def test_card_set_question(self):
+        new_question = 'What is e ?'
+        self.card.question = new_question
+        self.assertEqual(new_question, self.card.question)
+
+    def test_card_set_question_error(self):
+        bad_question = 4123451
+        self.assertRaises(TypeError, setattr, self.card.question, bad_question)
+
+    def test_card_set_answer(self):
+        new_answer = '2.71828'
+        self.card.answer = new_answer
+        self.assertEqual(new_answer, self.card.answer)
+
+    def test_card_set_answer_error(self):
+        bad_answer = 2.71828
+        self.assertRaises(TypeError, setattr, self.card.answer, bad_answer)
