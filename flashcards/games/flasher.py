9f580d20a4bee3a28d4a281bf923a322a02bd4d2
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2016-05-17 18:38:26 -0400

    A Flasher now takes a list of cards instead of a Set.

--- flashcards/games/flasher.py
+++ flashcards/games/flasher.py
@@ -1,15 +1,17 @@
+import random
+
 class CardFlasher(object):
     """
-    Contains the logic and the options behind the itteration on a StudySet.
+    Contains the logic and the options behind the itteration on a list of cards.
     """
 
-    def __init__(self, study_set):
+    def __init__(self, cards):
         """
         Creates the CardFlasher object.
 
-        :param study_set: The flashcards.sets.StudySet to enumerate through
+        :param cards: A list of BaseCard object
         """
-        self._study_set = study_set
+        self._cards = cards
 
         # Initialize the Flasher to start at the first card
         self._current_card = 0
@@ -21,13 +23,13 @@ class CardFlasher(object):
         :returns: The card object that is currently selected.
         """
         index = self._current_card
-        return self._study_set.get(index)
+        return self._cards[index]
 
     def next(self):
         """
         Set the selection to the next card in the set.
         """
-        if self._current_card + 1 < len(self._study_set._cards):
+        if self._current_card + 1 < len(self._cards):
             self._current_card += 1
 
     def previous(self):
@@ -36,3 +38,9 @@ class CardFlasher(object):
         """
         if not self._current_card - 1 < 0:
             self._current_card -= 1
+
+    def randomize(self):
+        """
+        Randomize the order of the cards.
+        """
+        random.shuffle(self._cards)
--- tests/test_flasher.py
+++ tests/test_flasher.py
@@ -1,6 +1,5 @@
 import unittest
 
-from flashcards.sets import StudySet
 from flashcards.cards import QuestionCard
 from flashcards.games.flasher import CardFlasher
 
@@ -9,27 +8,21 @@ class TestGameEnumerate(unittest.TestCase):
 
     def setUp(self):
 
-        # Create the study set test
-        self.study_set = StudySet('Some title')
-
         # Add some cards to the study set
         self.cards = [
             QuestionCard('q1', 'a1'),
             QuestionCard('q2', 'a2')
         ]
 
-        self.study_set.add(self.cards[0])
-        self.study_set.add(self.cards[1])
-
     def test_StudySetEnumerator(self):
 
         # Create the enumerator to enumerate this set
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
         self.assertIsNotNone(flasher)
 
     def test_get_current_card(self):
 
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
 
         # By default, the flasher starts at card index 0
         card = flasher.get_current_card()
@@ -37,7 +30,7 @@ class TestGameEnumerate(unittest.TestCase):
 
     def test_next(self):
 
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
 
         # Set the index of the current card to the next card
         flasher.next()
@@ -47,7 +40,7 @@ class TestGameEnumerate(unittest.TestCase):
 
     def test_next_out_of_bound(self):
 
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
 
         # In this case, the StudySet now only contains 2 card. Calling next
         # three times should not trigger an error and should return the last
@@ -61,7 +54,7 @@ class TestGameEnumerate(unittest.TestCase):
 
     def test_previous(self):
 
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
 
         # Set the index of the current card to the next card --> 1
         flasher.next()
@@ -73,10 +66,19 @@ class TestGameEnumerate(unittest.TestCase):
 
     def test_previous_out_of_bound(self):
 
-        flasher = CardFlasher(self.study_set)
+        flasher = CardFlasher(self.cards)
 
         # Flasher starts at index 0. Calling previous should stay at index 0
         flasher.previous()
 
         card = flasher.get_current_card()
         self.assertEqual(card, self.cards[0])
+
+    def test_randomize(self):
+
+        flasher = CardFlasher(self.cards)
+        flasher.randomize()
+
+        #TODO: Mock that shuffle has been called with the list of cards
+        self.assertIn(self.cards[0], flasher._cards)
+        self.assertIn(self.cards[1], flasher._cards)
