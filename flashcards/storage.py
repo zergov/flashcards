14d707480a34d01c9f895dcfc53b22ef98f53857
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2016-06-09 08:25:48 -0400

    Move FileStorage classes in utils/storage.py

--- flashcards/storage.py
+++ flashcards/storage.py
@@ -1,6 +1,6 @@
 import os
-import json
 
+from flashcards.utils import storage as storageUtils
 from flashcards import sets
 
 STORAGE_DIR_NAME = '.flashcards'
@@ -40,124 +40,7 @@ def store_study_set(study_set):
     storage_item.save(study_set)
 
 
-class FileStorage(object):
-    """
-    Utility object that manage the reading / saving of a file while assuring
-    that the file is closed once we're done using it.
-    """
-    def __init__(self, filepath):
-        self._filepath = filepath
-
-    def load(self):
-        """
-        Load and return the data stored in this file.
-
-        :returns: dictionary object.
-        """
-        return self._load_raw_content()
-
-    def _load_raw_content(self):
-        """Read and return the raw data in this file."""
-        self._assert_valid_file()
-
-        content = None
-        with open(self._filepath, 'r') as file:
-            content = file.read()
-
-        return content
-
-    def save(self, content):
-        """
-        Save new content in this file.
-
-        :param content: new content to store in this file.
-        """
-        self._save_raw_content(content)
-
-    def _save_raw_content(self, content):
-        """
-        Open and write the provided data in this file.
-        Overwriting the data already stored in this file.
-
-        :param content: the content to store in this file.
-        """
-        self._assert_valid_file()
-
-        with open(self._filepath, 'w') as file:
-            file.write(content)
-
-    def _assert_valid_file(self):
-        """
-        Raise an exception if the file at the given path is not a file or
-        does not exists.
-        """
-        if not os.path.isfile(self._filepath):
-            raise IOError("path: %s is not a file" % self._filepath)
-        if not os.path.exists(self._filepath):
-            raise IOError("path: %s does not exists" % self._filepath)
-
-    def _rename_filename(self, filename):
-        """
-        Without changing the path and the extension, rename the
-        base name of the file.
-
-        :param filename: the new name for this file.
-        """
-        directory = os.path.dirname(self._filepath)  # keep the same path
-        extension = os.path.splitext(self._filepath)[1]  # keep the extension
-
-        # Concatenate the new path for the file, rename the file and update the
-        # _filepath variable.
-        new_path = os.path.join(directory, filename + extension)
-        os.rename(self._filepath, new_path)
-        self._filepath = new_path
-
-
-class JSONFileStorage(FileStorage):
-    """
-    Utility object to save and load serialized JSON objects stored in a file.
-    """
-    def load(self):
-        """
-        Load and serialize the data in this file.
-
-        :returns: a dictionary object
-        """
-        content = self._load_raw_content()
-        return self._deserialize(content)
-
-    def save(self, content):
-        """
-        Serialize and save the content in this file.
-
-        :param content: the content to save
-        """
-        content = self._serialize(content)
-        self._save_raw_content(content)
-
-    def _serialize(self, data):
-        """
-        Serializing function used by this module.
-
-        :param data: the dictionary to serialize
-
-        :returns: a string of representation of the serialized object
-        """
-        return json.dumps(data, sort_keys=False, indent=4,
-                          separators=(',', ': '))
-
-    def _deserialize(self, string):
-        """
-        Deserializing function used by this module.
-
-        :param string: the string to deserialize
-
-        :returns: the dictionary representation of the deserialized string
-        """
-        return json.loads(string)
-
-
-class StudySetStorage(JSONFileStorage):
+class StudySetStorage(storageUtils.JSONFileStorage):
     """
     Utility object to load and save a StudySet object from a
     file on the machine.
@@ -177,29 +60,8 @@ class StudySetStorage(JSONFileStorage):
         super(StudySetStorage, self).save(data)
 
         # rename the name of this file by the title of this study set.
-        filename = _generate_filename_from_str(study_set.title)
+        filename = storageUtils.generate_filename_from_str(study_set.title)
         self._rename_filename(filename)
-
-
-def _generate_filename_from_str(string):
-    """
-    Generate a valid filename from a given string.
-
-    - replace all spaces and dashes with underscore.
-    - only keeps alphanumerical chars
-
-    :param string: the string to create the filename from
-
-    :returns: the generated string, a valid filename
-    """
-    keepchars = [' ', '-', '_']  # characters to keep in the filename
-    swapchars = {' ': '_', '-': '_'}  # keys are swapped by their values
-
-    for key, value in swapchars.items():
-        string = string.replace(key, value)
-
-    _ = [c for c in string if c.isalnum() or c is ' ' or c in keepchars]
-    return ''.join(_).rstrip()
 
 
 def _get_storage_path():
@@ -222,6 +84,6 @@ def _generate_study_set_filepath(study_set):
 
     :returns: absolute file path to the storage file.
     """
-    filename = _generate_filename_from_str(study_set.title)
+    filename = storageUtils.generate_filename_from_str(study_set.title)
     filename = filename + STUDY_SET_EXTENSION
     return os.path.join(_get_study_set_storage_path(), filename)
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -2,6 +2,7 @@ import unittest
 import mock
 
 from flashcards import storage
+from flashcards.utils import storage as storageUtils
 from flashcards.storage import StudySetStorage
 from flashcards.sets import StudySet
 from flashcards.cards import StudyCard
@@ -16,17 +17,17 @@ class TestStorageModule(unittest.TestCase):
         self.assertEqual('/home/admin/.flashcards', storage._get_storage_path())
 
     def test_generate_filename_from_str_spaces(self):
-        result = storage._generate_filename_from_str('file name')
+        result = storageUtils.generate_filename_from_str('file name')
         expected = 'file_name'
         self.assertEqual(expected, result)
 
     def test_generate_filename_from_str_dashes(self):
-        result = storage._generate_filename_from_str('file-name')
+        result = storageUtils.generate_filename_from_str('file-name')
         expected = 'file_name'
         self.assertEqual(expected, result)
 
     def test_generate_filename_from_str_alphanum(self):
-        result = storage._generate_filename_from_str('!f@i#l$e%-^n&a*m(e)0')
+        result = storageUtils.generate_filename_from_str('!f@i#l$e%-^n&a*m(e)0')
         expected = 'file_name0'
         self.assertEqual(expected, result)
 
@@ -50,7 +51,7 @@ class TestStoredStudySet(unittest.TestCase):
         study_set = StudySet('maths145', 'Maths questions')
         study_set.add(StudyCard('2+2=?', '4'))
 
-        newname = storage._generate_filename_from_str(study_set.title)
+        newname = storageUtils.generate_filename_from_str(study_set.title)
         stored_set._rename_filename(newname)
 
         expected = '/home/usr/.flashcards/maths145.json'
