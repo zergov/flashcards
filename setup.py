dc6db80f40529d9d85381be13c6c09391afe6fba
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2016-06-10 05:52:18 -0400

    Update requirements files.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from setuptools import find_packages
 DESCRIPTION = ("CLI application that focus on quickly creating "
                "study flash cards")
 
+REQUIREMENTS = ['click==6.6']
+
 setup(
     name='flashcards',
     author='Jonathan Lalande',
     author_email='jonathan.lalande.1@ens.etsmtl.ca',
     description=DESCRIPTION,
     packages=find_packages(),
+    install_requires=REQUIREMENTS,
     entry_points="""
         [console_scripts]
         flashcards=flashcards.main:cli
