beb64c33831dc8dfb0d9efe3769a9ba58cafa886
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2016-06-17 18:58:07 -0400

    Add -e option to cards.add command.
    When -e option is given, the prompt for the questions and the answers
    will be made in an editor instead of a standard prompt in the command
    line.

--- flashcards/commands/cards.py
+++ flashcards/commands/cards.py
@@ -4,6 +4,10 @@ flashcards.commands.cards
 
 Contains the commands and subcommands related to the cards resource.
 """
+import os
+import tempfile
+from subprocess import call
+
 import click
 
 from flashcards import storage
@@ -17,27 +21,61 @@ def cards_group():
 
 
 @click.command('add')
-@click.option('--question', prompt='Question')
-@click.option('--answer', prompt='Answer')
-def add(question, answer):
+@click.option('-q', '--question', default=None)
+@click.option('-a', '--answer', default=None)
+@click.option('-e', 'editormode', flag_value=True, default=False)
+def add(question, answer, editormode):
     """ Add a studycard to the currently selected studyset. """
-
-    # Load the currently selected studyset
     try:
         studyset = storage.load_selected_studyset()
 
-        # Create the card and add it to the studyset
-        card = StudyCard(question, answer)
-        studyset.add(card)
+        if question is None:
+            question = _ask_for_question(editormode)
 
+        if answer is None:
+            answer = _ask_for_answer(editormode)
+
+        # Create the card and add it to the studyset
+        studyset.add(StudyCard(question, answer))
         # Update the studyset by overwriting the old information.
         storage.store_studyset(studyset)
-
         click.echo('Card added to the studyset !')
 
     except IOError:
         click.echo('There is no studyset currently selected. '
                    'Select a studyset to add a card.')
 
+
+def _ask_for_question(editor_mode=False):
+    """
+    Prompt the user for a question.
+    """
+    return click.prompt('Question') if not editor_mode else prompt_via_editor()
+
+
+def _ask_for_answer(editor_mode=False):
+    """
+    Prompt the user for an answer.
+    """
+    return click.prompt('Answer') if not editor_mode else prompt_via_editor()
+
+
+def prompt_via_editor():
+    """
+    Open a temp file in an editor and return the input from the user.
+
+    :returns: the input str from the user.
+    """
+    editor = os.environ.get('EDITOR', 'vim')
+    data = ""
+
+    with tempfile.NamedTemporaryFile(suffix='.tmp') as f:
+        f.flush()
+        call([editor, f.name])
+
+        f.seek(0)
+        data = f.read()
+
+    return data
 
 cards_group.add_command(add)
